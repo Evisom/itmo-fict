c7e2189d0d819aef8f74563433efa65285c3363a
Dmitriy Shishmintsev <dmitriy@Dmitriys-MacBook-Air.local>
2023-02-20 23:59:34 +0300

    tests

--- 2_semester/ads/lab1/tests.py
+++ 2_semester/ads/lab1/tests.py
@@ -2,19 +2,25 @@ from bruteforce import bruteforceSearch
 from BoyerMoore import BoyerMooreSearch
 from KnuthMorrisPratt import KnuthMorrisPrattSearch
 from RabinKarp import RabinKarpSearch
+import timeit
 
+testfile = open('test.txt').readline()
 tests = [
-    ['ЭТОИЭТОТ', 'ТОТ'],
-    ['aaabab', 'b'],
-    ['aefdgaabadfgagdgdab', 'ab'],
-    ['asfgdshgrtsfdgfasshtdsasf', 'as'],
-    ['fdsgsndhtryegfd', 'dsf'],
-    ['sdfghjhkfuyjthdgfgnmhfkyturytesfdgfbgshrg','y']
+    [testfile, 'ab']
+    # ['ЭТОИЭТОТ', 'ТОТ'],
+    # ['aaabab', 'b'],
+    # ['aefdgaabadfgagdgdab', 'ab'],
+    # ['asfgdshgrtsfdgfasshtdsasf', 'as'],
+    # ['fdsgsndhtryegfd', 'dsf'],
+    # ['sdfghjhkfuyjthdgfgnmhfkyturytesfdgfbgshrg','y']
 ]
 
 for i in tests:
-    if ( bruteforceSearch(i[0], i[1]) == BoyerMooreSearch(i[0], i[1]) and BoyerMooreSearch(i[0], i[1]) == RabinKarpSearch(i[0], i[1]) and RabinKarpSearch(i[0], i[1]) ==  KnuthMorrisPrattSearch(i[0], i[1])):
-        print('✅', KnuthMorrisPrattSearch(i[0], i[1]))
-    else: 
-        print('❌')
-    print()
+    n = 100
+    print('Average time for 100 iterations')
+    print('String length - ', len(testfile), 'symbols')
+    print('bruteforce: ', timeit.timeit("bruteforceSearch(i[0], i[1])", globals=globals(), number=n)/n)
+    print('Boyer-Moore: ', timeit.timeit("BoyerMooreSearch(i[0], i[1])", globals=globals(), number=n)/n)
+    print('Rabin-Karp: ', timeit.timeit("RabinKarpSearch(i[0], i[1])", globals=globals(), number=n)/n)
+    print('Knuth-Morris-Pratt: ', timeit.timeit("KnuthMorrisPrattSearch(i[0], i[1])", globals=globals(), number=n)/n)
+    
