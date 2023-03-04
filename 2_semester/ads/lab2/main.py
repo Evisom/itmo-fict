d3944f177f65c54353ffd689fc8105fff0d77a37
Dmitriy Shishmintsev <dmitriy@Dmitriys-MacBook-Air.local>
2023-03-04 20:58:51 +0300

    ads lab fix

--- 2_semester/ads/lab2/main.py
+++ 2_semester/ads/lab2/main.py
@@ -1,120 +1,126 @@
 import math 
 import copy 
+import binascii
 
-def StringToArray(string: str) -> list:
+def StringToArray(string: str) -> list: # Эта функция представляет строку как массив из их ASCII кодов
     arr = []
     for i in string:
         arr.append(ord(i))
     return arr 
 
-
-def multiplicative_hash(arr: list) -> list:
-    M = len(arr)
-    C = (5**(1/2)-1)/2
+def multiplicative_hash(arr: list) -> list: # Функция для хэширования методом умножения
+    M = len(arr) 
+    C = (5**(1/2)-1)/2 # Оптимальная константа которую вывел Дональд Кнут
     arr = StringToArray(arr)
     hash = []
-    for i in arr:
+    for i in arr: # Хэшируем каждую букву
         h = round(M*((i*C)%1))
         hash.append(str(h))
-    hash = "".join(hash)
+    hash = "".join(hash) # Объединяем получившийся массив в одну строку
     return hash 
 
-def MD5_hash(string: str) -> bytearray:
+def MD5_hash(string: str) -> str:
     # эх если бы мне читали лекции по алгоритмам
 
-    # Round functions
-    def F(x,y,z): 
-        x = int(x, 2)
-        y = int(y, 2)
-        z = int(z, 2)
-        return bin(x & y | ~x & z).replace("-","")[2:]
+    # Функции F, G, H, I - функции для каждого раунда
+    # Используем побитовые логические операции
+    def F(x: int,y: int,z: int) -> int: 
+        result = (x & y | ~x & z)
+        return result 
     
-    def G(x,y,z):
-        x = int(x, 2)
-        y = int(y, 2)
-        z = int(z, 2)
-        return bin(x & z | y & ~z ).replace("-","")[2:]
+    def G(x: int,y: int,z: int) -> int: 
+        result = x & z | y & ~z 
+        return result 
     
-    def H(x,y,z):
-        x = int(x, 2)
-        y = int(y, 2)
-        z = int(z, 2)
-        return bin(x ^ y ^ z).replace("-","")[2:]
+    def H(x: int,y: int,z: int) -> int: 
+        result = x ^ y ^ z
+        return result 
     
-    def I(x,y,z):
-        x = int(x, 2)
-        y = int(y, 2)
-        z = int(z, 2)
-        return (bin(y ^ (x | ~ z))).replace("-","")[2:]
+    def I(x: int,y: int,z: int) -> int: 
+        result = y ^ (x | ~ z)
+        return result 
     
-    def addition(x,y, m=2**32):
-        x = int(x, 2)
-        y = int(y, 2)
+    def addition(x: int,y: int, m=2**32) -> int: # Функция для сложения по модулю 2**32
         z = (x+y) % m 
-        return bin(z)[2:]
+        return z 
 
-    def shift(x, s):
-        x = int(x, 2)
-        return bin(x<<2).replace("-","")[2:]
-
-
-    # 512 bits 
-    # 448 bits - body 
-    # 64 - length 
+    def shift(x: int, s: int) -> int: # Функция для битового сдвига
+        x = bin(x)[2:]
+        x = x[s:] + x[:s]
+        return int(x,2)
     
-    message = bin(0)
-    
+
+    # Ниже мы составим строку из 16тиричных представлений ASCII кодов каждого символа 
+    message = ''
     for i in string:
-        message+=bin(ord(i))[2:]
-    message+=bin(1)[2:]
-    message+= (bin(0)[2:]*(448 - len(message)%512 + 2))
-    length = bin(len(message)-2)[2:]
-    message = message + (bin(0)[2:] * (64 - len(length))) + length
-    message = message[2:]
-    
-    # Buffers 
-    A = bin(1732584193)[2:]
-    B = bin(4023233417)[2:]
-    C = bin(2562383102)[2:]
-    D = bin(10325476)[2:]
-    buffers = [A, B, C, D]
+        message+=hex(ord(i))[2:] #
 
-    S = [ # Shift values
+    length = str(hex(len(message)*4)) # Сохраним длину исходной строки в битах
+    message += '8' # Добавим 1 в конце (8_16 = 1000_2)
+    message += ('0' *(112 - len(message)%128 +4)) # Добавим нули в конец
+    message= message + '0' * (16-len(length)) + length[2:] # Сформируем сообщение длинной 512 бит
+    
+    buffers = [ # Буферы с их начальными значениями 
+        0x12AC2375, # A
+        0x3B341042, # B
+        0x5F62B97C, # C
+        0x4BA763E   # D
+    ]
+
+    S = [ # Значения дли сдвигов в каждом раунде
         [7,12,17,22],
         [5,9,14,20],
         [4,11,14,23],
         [6,10,15,21]
     ]
 
-    P = [F, G, H, I]
+    P = [F, G, H, I] # Для удобства массив с функциями для каждого раунда
 
-    T = [] # Constants 
-
+    T = [] # Константы  
     for i in range(1,65):
-        T.append(bin(round(2**32 * abs(math.sin(i))))[2:])
+        T.append(round(2**32 * abs(math.sin(i))))
+
+    blocks = []
+    for i in range(2, len(message), 128): # Разобьем сообщение на блоки длинной 512 бит
+        blocks.append(message[i:i+128])
+    
+    for b in range(0, len(blocks)): # Разобьем каждый 512битный блок на 16 32х битных блоков
+        block = copy.deepcopy(blocks[b])
+        blocks[b] = []
+        for i in range(0, 128, 8):
+            blocks[b].append(int(block[i:i+8], 16))
+
 
     iteration = 0
-    for b in range(0, len(message), 512):
-        block512 = (message[b:b+513])
-        for r in range(0, 4):
-            for i in range(0, 16):
-                block = (block512[i*16:i*16+17])
-                bufferCopy = copy.deepcopy(buffers)
-                processResult = P[r](buffers[1], buffers[2], buffers[3]) 
+    for b in range(0, len(blocks)): # Перебираем каждый 512 битный блок 
+        for r in range(0, 4): # 4 раунда
+            for i in range(0, len(blocks[b])): # Перебираем каждый из 16ти 32х битных блоков
+                bufferCopy = copy.deepcopy(buffers) # Копируем начальные значения буфера
+                processFunction = P[r](buffers[1], buffers[2], buffers[3]) # Получаем значение для фукнции текущего раунда
+                buffers[0] = addition(buffers[0], processFunction) # Добавляем значение функции к буферу А
+                buffers[0] = addition(buffers[0], blocks[b][i]) # Добавляем значение текущего 32х битного блока к буферу А
+                buffers[0] = addition(buffers[0], T[iteration]) # Добавляем соответствующую константу к буферу А
+                buffers[0] = shift(buffers[0], S[r][i//4]) # Делаем побитовый сдвиг для буфера А
+                # Константы которые хранятся в массиве S я нагуглил 
+                # Они рассчитаны каким-то умным путем, но дано всего 16 констант
+                # Каждые 4 константы для каждого из 4х раундов, но в каком порядке их использовать я не разобрался
+                # Тут я использую константу по i%4, так как посчитал, что так логичнее всего
+                buffers[0] = addition(buffers[0], buffers[1]) # Добавляем к буферу А изначальное значение буфера А
+                buffers = buffers[3:4] + buffers[0:3] # Перемещаем наши буферы (ABCD => DABC => CDAB => BCDA => ABCD)
+                iteration+=1 # Счетчик итераций чисто что бы константы T прибавлять легче было 
 
-                buffers[0] = addition(buffers[0], processResult)
-                buffers[0] = addition(buffers[0], block)
-                buffers[0] = addition(buffers[0], T[iteration-1])
-                buffers[0] = shift(buffers[0], S[r][i%4])
-                buffers[0] = addition(buffers[0], buffers[1])
+    # По результату выполнения кода выше имеем наш хэш в массиве buffers в десятиричной системе счисления
 
-                buffers = buffers[3:4] + buffers[0:3]
-                iteration+=1
 
-    hex_string = hex(int("".join(buffers), 2))
+    hex_string = '0x'
+    for i in buffers: # Проходимся по всем буферам
+        hi = hex(i)[2:] # Перевод из 10 в 16 систему счисления
+        if len(hi) < 8: # Проверяем если не хватает нулей в конце, если что добавляем
+            hi = '0'*(8-len(hi)) + hi 
+        hex_string+=hi 
 
-    return hex_string
-
+    return hex_string # Возвращаем наш хэш в шестнадцатиричном формате
+    
 text = input()
 print('Multiplicative hash: ', multiplicative_hash(text))
 print('MD5 hash: ', MD5_hash(text))
